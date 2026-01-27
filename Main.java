f4032a6f2058ccf871e0ebb185590d605e47e9da
paraisoamir76-coder <paraisoamir76@gmail.com>
2026-01-27 23:58:19 +0800

    ADDITION
    implemented addition logic

--- Main.java
+++ Main.java
@@ -7,11 +7,23 @@ public class Main{
         int num2 = 0;
         int result = 0;
 
+        System.out.println("OPERATORS");
+        System.out.println("1. Addition");
+        int choice = scan.nextInt();
+
         System.out.print("Enter First Number: ");
         num1 = scan.nextInt();
         System.out.print("Enter Second Number: ");
         num2 = scan.nextInt();
 
+        switch (choice){
+            case 1 -> result = addition(num1, num2);
+        }
+
         scan.close();
     }
+    public static int addition(int num1, int num2){
+        return num1 + num2;
+    }
+
 }
